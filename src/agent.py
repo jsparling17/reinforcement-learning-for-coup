906e629ff93cc0a521fd62b83d78c5f69fbe9ee1
Jonah Sparling <jonah.sparling@gmail.com>
2024-04-19 21:57:11 -0400

    network size

--- src/agent.py
+++ src/agent.py
@@ -29,9 +29,9 @@ class DQN(nn.Module):
     def __init__(self, state_size: int, action_count: int):
         super(DQN, self).__init__()
 
-        self.layer1 = nn.Linear(state_size, 100)
-        self.layer2 = nn.Linear(100, 100)
-        self.layer3 = nn.Linear(100, action_count)
+        self.layer1 = nn.Linear(state_size, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, action_count)
 
     def forward(self, x):
         x = F.relu(self.layer1(x))
